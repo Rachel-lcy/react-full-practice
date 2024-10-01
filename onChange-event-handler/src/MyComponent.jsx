7d507e9181792a305b9bd30bee2d637658bf6d2e
Rachel <hurui.lcy@gmail.com>
2024-09-30 21:16:21 -0600

    color-picker-program

--- onChange-event-handler/src/MyComponent.jsx
+++ onChange-event-handler/src/MyComponent.jsx
@@ -34,34 +34,32 @@ function MyComponent(){
   }
 
   return(
-  <div>
-      <input type="text" value={name} onChange={handleNameChange}/>
-      <p>Name: {name}</p>
+    <div>
+        <input type="text" value={name} onChange={handleNameChange}/>
+        <p>Name: {name}</p>
 
-      <input type="number" value={quantity} onChange={handleQuantityChange}/>
-      <p>Quantity: {quantity}</p>
+        <input type="number" value={quantity} onChange={handleQuantityChange}/>
+        <p>Quantity: {quantity}</p>
 
-      <textarea value = {comment} onChange={handleCommentChange} placeholder='Enter delivery instructions'></textarea>
-      <p>Comment: {comment} </p>
+        <textarea value = {comment} onChange={handleCommentChange} placeholder='Enter delivery instructions'></textarea>
+        <p>Comment: {comment} </p>
 
-      <select  value={payment} onChange={handlePaymentChange}></select>
-      <option value="">Select an option</option>
-      <option value="Visa">Visa</option>
-      <option value="MasterCard">MasterCard</option>
-      <option value="GiftCard">GiftCard</option>
-      <p>Payment :{payment}</p>
+        <select  value={payment} onChange={handlePaymentChange}></select>
+        <option value="">Select an option</option>
+        <option value="Visa">Visa</option>
+        <option value="MasterCard">MasterCard</option>
+        <option value="GiftCard">GiftCard</option>
+        <p>Payment :{payment}</p>
 
     <label htmlFor="">Pick up
-      <input type="radio" value ="pick up" checked = {shipping === "pick up"} onChange={handleShippingChange}/>
+        <input type="radio" value ="pick up" checked = {shipping === "pick up"} onChange={handleShippingChange}/>
     </label><br/>
     
     <label htmlFor="">Delivery
-      <input type="radio" value ="delivery" checked = {shipping === "delivery"} onChange={handleShippingChange}/>
+        <input type="radio" value ="delivery" checked = {shipping === "delivery"} onChange={handleShippingChange}/>
     </label>
-    <p>Shipping : {shipping}</p>
-
-
-  </div>);
+      <p>Shipping : {shipping}</p>
+    </div>);
   
 }
 export default MyComponent
